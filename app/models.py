4d3a013cd0ebccaf16941281a8d274695750f9a5
DoctorLector <roller97@yandex.ru>
2022-07-19 19:47:00 +0300

    add authorization form

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 family = db.Table('family',
                   db.Column('id_user', db.Integer, db.ForeignKey('users.id')),
                   db.Column('id_partner', db.Integer, db.ForeignKey('users.id'))
                 )
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -21,6 +23,16 @@ class Users(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
 
 class Priority(db.Model):
     id = db.Column(db.Integer, primary_key=True)
