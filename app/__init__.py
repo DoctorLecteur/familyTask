a95e26d6ef2d9485044b3ae6f267d0a921e31648
DoctorLector <roller97@yandex.ru>
2022-09-04 20:32:24 +0300

    add error template and logger error without email

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,5 +19,22 @@ bootstrap = Bootstrap(app)
 mail = Mail(app)
 moment = Moment(app)
 
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_SERVER'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr=app.config['ADMINS'],
+            toaddrs=app.config['ADMINS'], subject='FamilyTask Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
-from app import routes, models
+
+from app import routes, models, errors
