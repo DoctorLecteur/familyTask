218d3e08370e1a014de3b7eab9a4beb16e4a2002
DoctorLector <roller97@yandex.ru>
2022-09-14 21:41:53 +0300

    debug push notify

--- app/routes.py
+++ app/routes.py
@@ -511,6 +511,7 @@ def send_push_notification():
         "title": data_param["title"],
         "body": data_param["body"]
     })
+    print('data_param', data_param)
     data_param_push_json_endpoint = json.loads(data_param["param"])["endpoint"]
     id_user_partner = current_user.get_id_by_username(current_user.get_partner(current_user))
     user_subscription = Subscription.query.filter_by(id_users=id_user_partner).all()
