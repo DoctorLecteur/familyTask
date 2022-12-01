49fd337f426952f56e377d10616387363df7b449
DoctorLector <roller97@yandex.ru>
2022-12-01 22:35:52 +0300

    add type_task second

--- app/routes.py
+++ app/routes.py
@@ -133,7 +133,7 @@ def family():
 @app.route('/tasks', methods=['GET', 'POST'])
 @login_required
 def tasks():
-    type_task = TypeTask(name='Задача')
+    type_task = TypeTask(name='Подзадача')
     db.session.add(type_task)
     db.session.commit()
     form = EmptyForm()
